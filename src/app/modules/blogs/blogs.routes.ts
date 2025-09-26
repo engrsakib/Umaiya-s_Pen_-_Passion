e163b650fabb04b5989e1193a27a8d5275c6a77c
engrsakib <snazmussakib01@gmail.com>
2025-09-26 16:15:29 +0600

    all counts fully complete

--- src/app/modules/blogs/blogs.routes.ts
+++ src/app/modules/blogs/blogs.routes.ts
@@ -6,10 +6,10 @@ import { BlogController } from "./blogs.controller";
 import { createBlogZodSchema } from "./blogs.validations";
 
 const router = Router();
+router.get("/count", BlogController.getAllCount);
 router.post("/create",verifyToken(role.ADMIN, role.SUPER_ADMIN),validateRequest(createBlogZodSchema), BlogController.createBlog);
 router.get("/", BlogController.getAllBlogs);
 router.get("/:slug", BlogController.getSingleBlog);
 router.patch("/:id", verifyToken(role.ADMIN, role.SUPER_ADMIN), validateRequest(createBlogZodSchema), BlogController.updateBlog);
 router.delete("/:id", verifyToken(role.ADMIN, role.SUPER_ADMIN), BlogController.deleteBlog);
-
 export const BlogsRoutes = router;
